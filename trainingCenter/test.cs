69cf2fd5ef511f29680bf61e97fa812b33c763e9
magdy <mohamedmagdy171199@gmail.com>
2023-02-27 16:07:19 +0200

    add test class

---
+++ trainingCenter/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trainingCenter
+{
+    internal class test
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
