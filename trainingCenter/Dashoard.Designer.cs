792e04d5910549fcaf292ec2611ebc48233db640
kareem-24 <kamkareem58@gmail.com>
2023-03-08 01:09:21 +0200

    add images

--- trainingCenter/Dashoard.Designer.cs
+++ trainingCenter/Dashoard.Designer.cs
@@ -32,6 +32,20 @@
             this.metroSetControlBox1 = new MetroSet_UI.Controls.MetroSetControlBox();
             this.label4 = new System.Windows.Forms.Label();
             this.metroSetButton4 = new MetroSet_UI.Controls.MetroSetButton();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -67,7 +81,7 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(853, 70);
+            this.label4.Location = new System.Drawing.Point(853, 24);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(365, 42);
             this.label4.TabIndex = 1;
@@ -83,7 +97,7 @@
             this.metroSetButton4.HoverColor = System.Drawing.Color.Transparent;
             this.metroSetButton4.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
             this.metroSetButton4.IsDerivedStyle = false;
-            this.metroSetButton4.Location = new System.Drawing.Point(33, 31);
+            this.metroSetButton4.Location = new System.Drawing.Point(33, 16);
             this.metroSetButton4.Name = "metroSetButton4";
             this.metroSetButton4.NormalBorderColor = System.Drawing.Color.Transparent;
             this.metroSetButton4.NormalColor = System.Drawing.Color.Transparent;
@@ -99,15 +113,198 @@
             this.metroSetButton4.ThemeAuthor = "Narwin";
             this.metroSetButton4.ThemeName = "MetroLite";
             // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button12.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.Color.White;
+            this.button12.Image = ((System.Drawing.Image)(resources.GetObject("button12.Image")));
+            this.button12.Location = new System.Drawing.Point(24, 506);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(226, 199);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "\r\n\r\n\r\n\r\n\r\n\r\nإجمالى التعاملات المالية";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button11.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.White;
+            this.button11.Image = ((System.Drawing.Image)(resources.GetObject("button11.Image")));
+            this.button11.Location = new System.Drawing.Point(274, 506);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(226, 199);
+            this.button11.TabIndex = 5;
+            this.button11.Text = "\r\n\r\n\r\n\r\n\r\n\r\nإجمالى التعاملات المالية";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button10.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.White;
+            this.button10.Image = global::trainingCenter.Properties.Resources.money_bag;
+            this.button10.Location = new System.Drawing.Point(524, 506);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(226, 199);
+            this.button10.TabIndex = 5;
+            this.button10.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسعيرة الخدمات";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button6.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = global::trainingCenter.Properties.Resources.wallet;
+            this.button6.Location = new System.Drawing.Point(774, 506);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(226, 199);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "\r\n\r\n\r\n\r\n\r\n\r\nإجمالى التعاملات المالية";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button8.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Image = global::trainingCenter.Properties.Resources.wallet;
+            this.button8.Location = new System.Drawing.Point(1021, 506);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(226, 199);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "\r\n\r\n\r\n\r\n\r\n\r\nالمصروفات والإيرادات";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button15.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.ForeColor = System.Drawing.Color.White;
+            this.button15.Image = ((System.Drawing.Image)(resources.GetObject("button15.Image")));
+            this.button15.Location = new System.Drawing.Point(454, 289);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(186, 199);
+            this.button15.TabIndex = 5;
+            this.button15.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل الحضور";
+            this.button15.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button14.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.ForeColor = System.Drawing.Color.White;
+            this.button14.Image = ((System.Drawing.Image)(resources.GetObject("button14.Image")));
+            this.button14.Location = new System.Drawing.Point(656, 289);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(186, 199);
+            this.button14.TabIndex = 5;
+            this.button14.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل الحضور";
+            this.button14.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button13.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.Color.White;
+            this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
+            this.button13.Location = new System.Drawing.Point(860, 289);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(186, 199);
+            this.button13.TabIndex = 5;
+            this.button13.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل الحضور";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button9.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.Image = global::trainingCenter.Properties.Resources.attendance;
+            this.button9.Location = new System.Drawing.Point(1061, 289);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(186, 199);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل الحضور";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button4.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::trainingCenter.Properties.Resources._class;
+            this.button4.Location = new System.Drawing.Point(42, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 199);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل مجموعة";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button7.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Image = global::trainingCenter.Properties.Resources.calendar;
+            this.button7.Location = new System.Drawing.Point(250, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(186, 199);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل سنة دراسية";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button5.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = global::trainingCenter.Properties.Resources.profile;
+            this.button5.Location = new System.Drawing.Point(454, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 199);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل مستخدم";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::trainingCenter.Properties.Resources.teacher;
+            this.button3.Location = new System.Drawing.Point(656, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 199);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل مدرس";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::trainingCenter.Properties.Resources.book_stack;
+            this.button2.Location = new System.Drawing.Point(860, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 199);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل مادة";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(992, 133);
+            this.button1.Location = new System.Drawing.Point(1061, 75);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 205);
+            this.button1.Size = new System.Drawing.Size(186, 199);
             this.button1.TabIndex = 5;
             this.button1.Text = "\r\n\r\n\r\n\r\n\r\n\r\nتسجيل الطلاب\r\n";
             this.button1.UseVisualStyleBackColor = false;
@@ -120,6 +317,20 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(23)))), ((int)(((byte)(62)))));
             this.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(20)))), ((int)(((byte)(54)))));
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.metroSetButton4);
             this.Controls.Add(this.metroSetControlBox1);
@@ -139,6 +350,20 @@
         private System.Windows.Forms.Label label4;
         private MetroSet_UI.Controls.MetroSetButton metroSetButton4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
     }
 }
 
